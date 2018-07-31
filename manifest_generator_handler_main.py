865b494b26dc4734bf02d507b197cc769d2ba54b
Srinivas <srinivas.nannapaneni@reancloud.com>
2018-07-31 12:51:22 -0400

    DOTAS-333

--- manifest_generator_handler_main.py
+++ manifest_generator_handler_main.py
@@ -3,5 +3,10 @@ from lambdas.manifest_generator_lambda_handler import *
 
 def lambda_handler(event, context):
     # manifest_generator_handle_event = GenerateManifests()
-    batch_id = generate_manifest_files(event, context)
-    LoggerUtility.logInfo("Manifest files generated for batchId : "+batch_id)
+    data = generate_manifest_files(event, context)
+    LoggerUtility.logInfo("Manifest files generated for batchId : "+ data['batch_id'])
+    return data
+
+#lambda_handler({'table_type':'alert','is_historical':'true', 'batch_id' :'1531486938' },{})
+#lambda_handler({'table_type':'irregularity','is_historical':'true'},{})
+#lambda_handler({'table_type':'irregularity','is_historical':'true'},{})
