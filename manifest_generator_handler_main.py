726720677998a182f80afcaf86bb65ec9a5f3066
HannahKent <hkent@acrotron.com>
2019-10-08 23:19:26 -0600

    SDC-921 removed code smells

--- manifest_generator_handler_main.py
+++ manifest_generator_handler_main.py
@@ -1,12 +1,7 @@
 from lambdas.manifest_generator_lambda_handler import *
 
 
-def lambda_handler(event, context):
-    # manifest_generator_handle_event = GenerateManifests()
-    data = generate_manifest_files(event, context)
-    LoggerUtility.logInfo("Manifest files generated for batchId : "+ data['batch_id'])
+def lambda_handler(event, *args, **kwargs):
+    data = generate_manifest_files(event)
+    LoggerUtility.log_info("Manifest files generated for batchId : " + data['batch_id'])
     return data
-
-#lambda_handler({'table_type':'alert','is_historical':'true', 'batch_id' :'1531486938' },{})
-#lambda_handler({'table_type':'irregularity','is_historical':'true'},{})
-#lambda_handler({'table_type':'irregularity','is_historical':'true'},{})
