ed88941e72d4fe91f8dc5bf81139816af119603d
Hannah Kent <hkent@acrotron.com>
2019-11-11 11:43:06 -0700

    added context parameter back to lambda_handler

--- manifest_generator_handler_main.py
+++ manifest_generator_handler_main.py
@@ -1,7 +1,7 @@
 from lambdas.manifest_generator_lambda_handler import *
 
 
-def lambda_handler(event, *args, **kwargs):
+def lambda_handler(event, context):
     data = generate_manifest_files(event)
     LoggerUtility.log_info("Manifest files generated for batchId : " + data['batch_id'])
     return data
